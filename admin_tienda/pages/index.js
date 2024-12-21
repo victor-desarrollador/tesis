f5553af7175abc77f472044d3368d8c4288da412
Victor_Enrqiue_Molina <119140911+victor-desarrollador@users.noreply.github.com>
2024-12-20 21:43:18 -0300

    actualizacion 2
    creando componentes y paginas

--- admin_tienda/pages/index.js
+++ admin_tienda/pages/index.js
@@ -1,23 +1,22 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import Nav from "../components/Nav"
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
 
   const { data: session } = useSession();
-    if(!session){
-      return (
-        <div className="bg-blue-900 w-screen h-screen flex items-center">
-          <div className="text-center w-full">
-            <button onClickCapture={()=>signIn("google")} className="bg-white p-2 px-4 rounded-lg">Iniciar sesión con Google</button>
-          </div>
-        </div>
-      );
-    }
+  console.log(session);
 
-    return (
-      <div className="bg-blue-900 min-h-screen">
-          <Nav />
-          <div className="bg-white flex-grow mt-2 mr-2  mb-2 rounded-lg p-4 "> iniciado sesión como: {session.user.name}</div>
-        </div>
-    );
+  return <Layout>
+    <div className="text-blue-900 flex justify-between">
+      <h2>
+        hola <b>{session?.user?.name}</b>
+      </h2>
+      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+        <img src={session?.user?.image} alt="" className="w-10 h-10 " />
+        <span className=" px-2 ">
+          {session?.user?.name}
+        </span>
+      </div>
+    </div>
+  </Layout>
 }
